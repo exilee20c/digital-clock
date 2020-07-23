65aacb518ffc03609f8918c107fc01a1d038469b
Inhyuk Park <exilee20c@gmail.com>
2020-07-23 13:36:02 +0900

    디지털 완성 (1버전)

--- src/components/atoms/Digit/Digit.js
+++ src/components/atoms/Digit/Digit.js
@@ -2,8 +2,41 @@ import React from "react";
 import PropTypes from "prop-types";
 import c from "./Digit.module.scss";
 
+const ENTIRE_STATE_MATRIX = [
+  [1, 0, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 0, 1],
+  [1, 1, 1, 0, 1, 1, 0],
+  [1, 1, 1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 1, 0, 1],
+  [1, 1, 1, 1, 0, 0, 1],
+  [1, 1, 1, 1, 0, 1, 1],
+  [1, 0, 0, 1, 1, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 0, 1],
+];
+
 function Digit({ value }) {
-  return <div className={c["wrapper"]}>{value}</div>;
+  const stateMatrix = ENTIRE_STATE_MATRIX[value];
+
+  return (
+    <div className={c["wrapper"]}>
+      {new Array(3)
+        .fill("horizontal")
+        .concat(new Array(4).fill("vertical"))
+        .map((orientation, i) => (
+          <div
+            className={
+              c["dash"] +
+              " " +
+              c[orientation] +
+              " " +
+              c[`dash-${i + 1}`] +
+              (stateMatrix[i] ? " " + c["active"] : "")
+            }
+          />
+        ))}
+    </div>
+  );
 }
 
 Digit.defaultProps = { value: 0 };
